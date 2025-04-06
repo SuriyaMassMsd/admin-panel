bf0962be07c9b31a3aa940c70132e4194d0f4f5a
suriyamassmsd <ksmass2001@gmail.com>
2025-04-06 17:59:42 +0530

    table responsive

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -7,10 +7,11 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-// import EditIcon from "@mui/icons-material/Edit";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Animations from "./Skeleton/TableSkeleton";
 import LongMenu from "./UserSuspend";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -19,6 +20,10 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
+  },
+  [theme.breakpoints.down("sm")]: {
+    padding: "8px 6px",
+    fontSize: "12px",
   },
 }));
 
@@ -34,19 +39,42 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 export default function CustomizedTables(props) {
   const data = props?.data || [];
   const { setPathname } = props.current;
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   if (!props.data) return <Animations />;
 
   return (
-    <TableContainer component={Paper} sx={{ width: "100%", overflowX: "auto" }}>
-      <Table aria-label="customized table">
+    <TableContainer
+      component={Paper}
+      sx={{
+        width: "100%",
+        overflowX: "auto",
+        boxShadow: "none",
+        // border: "1px solid #e0e0e0",
+        [theme.breakpoints.down("sm")]: {
+          border: "none",
+        },
+      }}
+    >
+      <Table
+        aria-label="customized table"
+        sx={{
+          minWidth: isSmallScreen ? "auto" : 650,
+          [theme.breakpoints.down("sm")]: {
+            "& .MuiTableCell-root": {
+              padding: "6px 4px",
+            },
+          },
+        }}
+      >
         <TableHead>
           <TableRow>
-            <StyledTableCell>S.No</StyledTableCell>
-            <StyledTableCell>Name</StyledTableCell>
-            <StyledTableCell>Email</StyledTableCell>
+            {!isSmallScreen && <StyledTableCell>S.No</StyledTableCell>}
+            <StyledTableCell>{isSmallScreen ? "User" : "Name"}</StyledTableCell>
+            {!isSmallScreen && <StyledTableCell>Email</StyledTableCell>}
             <StyledTableCell>Role</StyledTableCell>
-            <StyledTableCell>Edit</StyledTableCell>
+            <StyledTableCell>Action</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -54,9 +82,16 @@ export default function CustomizedTables(props) {
             const { email, id, role } = item;
             return (
               <StyledTableRow key={id}>
-                <StyledTableCell>{index + 1}</StyledTableCell>
-                <StyledTableCell>{email.split("@")[0]}</StyledTableCell>
-                <StyledTableCell>{email}</StyledTableCell>
+                {!isSmallScreen && (
+                  <StyledTableCell>{index + 1}</StyledTableCell>
+                )}
+                <StyledTableCell>
+                  {isSmallScreen
+                    ? email.split("@")[0].substring(0, 12) +
+                      (email.split("@")[0].length > 12 ? "..." : "")
+                    : email.split("@")[0]}
+                </StyledTableCell>
+                {!isSmallScreen && <StyledTableCell>{email}</StyledTableCell>}
                 <StyledTableCell>{role}</StyledTableCell>
                 <StyledTableCell>
                   <div className="cursor-pointer">
