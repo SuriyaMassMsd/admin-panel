06bcf94cf13b0c19a9dc59ced043441c2758620c
suriyamassmsd <ksmass2001@gmail.com>
2025-05-06 13:43:55 +0530

    fixing vercel error

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,23 +1,29 @@
-const getAuthHeaders = () => {
-  const token = localStorage.getItem("token");
-  return {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+import useSWR from "swr";
+import axios from "axios";
+import useSWRMutation from "swr/mutation";
+
+const token = localStorage.getItem("token");
+const headers = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 };
 
 const fetcher = async (url) => {
-  const res = await axios.get(url, getAuthHeaders());
+  const res = await axios.get(url, headers);
   return res.data;
 };
 
 const deleteFetcher = async (url, { arg: id }) => {
-  const res = await axios.delete(`${url}/${id}`, getAuthHeaders());
+  const res = await axios.delete(`${url}/${id}`, headers);
   return res.data;
 };
 
 const postFetcher = async (url, { arg: payload }) => {
-  const res = await axios.post(url, payload, getAuthHeaders());
+  const res = await axios.post(url, payload, headers);
   return res.data;
 };
+
+export const getData = (url) => useSWR(url, fetcher);
+export const postData = (url) => useSWRMutation(url, postFetcher);
+export const delData = (url) => useSWRMutation(url, deleteFetcher);
